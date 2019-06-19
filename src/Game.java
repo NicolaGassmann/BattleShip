97a6630abf10edf0606ac0e762be3412ec06d56a
Teoman Lam <TeomanDoganay.Lam@bwz-rappi.ch>
2019-06-19 16:20:13 +0200

    Structure

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,21 @@
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+
 public class Game {
+
+
+    public Scene createScene(String styleSheet){
+        GridPane start_background = new GridPane();
+        Group root = new Group();
+        root.getChildren().add(start_background);
+        Scene scene = new Scene(root, 800, 600);
+        scene.getStylesheets().add(getClass().getResource(styleSheet).toExternalForm());
+
+        return scene;
+    }
+
+
+
 
 }
